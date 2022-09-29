c49b38f05ff9925685a1d797d0e4c418f9177580
ento0718 <114481782+ento0718@users.noreply.github.com>
2022-09-29 18:09:42 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -127,6 +127,40 @@ async def on_message(message):
                             index = random.randint(0, len(msgs)-1)
                             await message.channel.send(msgs[index].replace('<username>',username))
                     return
+                  
+                  
+                  
+const { Client, Intents, MessageEmbed } = require("discord.js")
+ 
+const client = new Client({
+    intents: [
+      Intents.FLAGS.GUILDS,
+      Intents.FLAGS.GUILD_MESSAGES
+    ]
+})
+ 
+client.on("ready", () => {
+    console.log("機器人已經成功上線且載入成功!")
+})
+ 
+client.on("messageCreate", (message) => {
+    const prefix = "/" // 如果要改成自己的前綴，改"裡面的字串
+    if (!message.content.startsWith(prefix))
+        return
+    
+    const args = message.content.slice(prefix.length).split(" ")
+    switch (args[0]) {
+        case "dice":
+            const final = Math.floor(Math.random() * (6 - 1)) + 1
+            const diceEmbed = new MessageEmbed()
+                .setTitle(`🎲 你得到了 ${final}`)
+                .setColor("#5865F2")
+            return message.reply({
+                embeds: [diceEmbed]
+            })
+    }
+})
+
     
 # Bot起動
 client.run(TOKEN)
